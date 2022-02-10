d2ff308154f76c51275cb72e67909df1d2523ee7
AizawaRen <aizawaren41@gmail.com>
2022-02-11 07:16:11 +0900

    1

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -35,7 +35,7 @@ public class LoginServlet extends HttpServlet {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/loginResult.jsp");
 			dispatcher.forward(request, response);
 		}else {
-			response.sendRedirect("/index.jsp");
+			response.sendRedirect("/timecard-app");
 		}
 	}
 
