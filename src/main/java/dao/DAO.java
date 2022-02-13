bc4136caacf9da967cbef385886756980c1641f2
AizawaRen <aizawaren41@gmail.com>
2022-02-14 07:10:24 +0900

    1

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -12,27 +12,24 @@ import model.LoginInfo;
 public class DAO {
 	
 	private final String JDBC_URL = "jdbc:postgresql://ec2-52-73-149-159.compute-1.amazonaws.com:5432/de3sgrkr4243o0?user=bqpygyzdwvkzvj&password=3fef9018117dca59473bac1c390b79236216cfa57b89d50aeadb3f80a05d51d1";
-	/*
-	 * private final String JDBC_URL =
-	 * "jdbc:postgresql://bqpygyzdwvkzvj:3fef9018117dca59473bac1c390b79236216cfa57b89d50aeadb3f80a05d51d1@ec2-52-73-149-159.compute-1.amazonaws.com:5432/de3sgrkr4243o0";
-	 */
 	private final String DB_USER = "bqpygyzdwvkzvj";
 	private final String DB_PASS = "3fef9018117dca59473bac1c390b79236216cfa57b89d50aeadb3f80a05d51d1";
 	
 	public Employee findByLogin(LoginInfo li){
 	
 		Employee emp = null;
+		Connection conn = null;
 		
 		try{
 		    Class.forName("org.postgresql.Driver");
 		    //
 		}
 		catch (ClassNotFoundException e) {
-		    //
+		    throw new IllegalStateException("ドライバのロードに失敗しました");
 		}
 	
 		try{
-			Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+			conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
 			
 			String sql = "SELECT * FROM employee WHERE enumber = ? AND pass = ?;";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
@@ -46,9 +43,18 @@ public class DAO {
 				String name = rs.getString("name");
 				emp = new Employee(number, pass, name);
 			}
+			
 		}catch (SQLException e){
 			e.printStackTrace();
 			return null;
+		}finally {
+			if(conn != null) {
+				try {
+					conn.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		
 		 return emp;
