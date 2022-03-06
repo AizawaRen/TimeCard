5098aa7e5af61c1e10184e89c9bbaa21a28c005e
AizawaRen <aizawaren41@gmail.com>
2022-03-07 02:26:34 +0900

    1

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,10 +1,13 @@
 package model;
+import java.io.Serializable;
 
-public class Employee {
+public class Employee implements Serializable{
 	private String number;
 	private String pass;
 	private String name;
 	private int yph;
+	
+	public Employee() {};
 	
 	public Employee(String number, String pass, String name, int yph) {
 		this.number = number;
